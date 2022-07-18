5d6caca10c83dd23ea2162dc8e13f837d31463e4
Arminas Keršys <arminas@ver.lt>
2022-07-18 21:59:21 +0300

    Content rename

--- build_release.sh
+++ build_release.sh
@@ -4,8 +4,8 @@ mkdir workspace
 set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SMA528B/SMT733_6G.dsc -b RELEASE
-gzip -c < workspace/Build/SMA528B/RELEASE_GCC5/FV/SMT733_UEFI.fd >uefi.img
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p SMA528B/SMA528B_6G.dsc -b RELEASE
+gzip -c < workspace/Build/SMA528B/RELEASE_GCC5/FV/SMA528B_UEFI.fd >uefi.img
 cat SMA528B.dtb >>uefi.img
 echo > ramdisk
 abootimg --create boot-SMA528B.img -k uefi.img -r ramdisk
